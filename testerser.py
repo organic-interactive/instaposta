6782113049f10c6cbbfd78e92b92766a1b0e847d
AJ Gayler <austingayler@gmail.com>
2016-01-31 15:04:37 -0700

    cleaned up merge

--- testerser.py
+++
@@ -1,7 +0,0 @@
-import urllib, re
-
-search_url = "https://500px.com/popular?categories=Nature,Landscapes"
-data = urllib.urlopen(search_url).read()
-f = open("stuff.html", "w")
-f.write(data)
-f.close()
