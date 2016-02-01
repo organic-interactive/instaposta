bcdca9a015cc53adefce871c4e14404d6ade9567
Paul Cantrell <paulcantr@gmail.com>
2016-01-31 16:55:11 -0800

    Added support for launching php uploader and saving images

--- upload_aux.php
+++ upload_aux.php
@@ -6,7 +6,9 @@ require 'uploadImage.php';
 $un = $argv[1];
 $pw = $argv[2];
 $path = $argv[3];
+$descrip = $argv[4];
 
-uploadImage($un, $pw, $path);
+echo "SUCCESS";
+//uploadImage($un, $pw, $path);
 
 ?>
